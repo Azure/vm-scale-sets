a99b05f0565dda7185d53e6db4f86b673c408cdd
Guy Bowerman <guybo@outlook.com>
2017-11-04 12:14:26 -0700

    Start instances as a single batch

--- hack2017/Scripts/vmssrds.ps1
+++ hack2017/Scripts/vmssrds.ps1
@@ -2,28 +2,34 @@ Param(
     [string]$resourceGroup,
     [string]$vmssName,
     [int]$startCount = 1
-  )
+)
 
 # variable to track the number of started instances
-$started = 0
+$count = 0
+$id_string = ''
 
 # get a list of VMSS VMs - note the -InstanceView argument is not working in list mode
 $vmss_vms = Get-AzureRmVmssVM -ResourceGroupName $resourceGroup -VMScaleSetName $vmssName
 
-ForEach ($vm in $vmss_vms) 
-{
+ForEach ($vm in $vmss_vms) {
     Write-Host 'Processing instance: ' $vm.InstanceID
     $vm_instance_view = Get-AzureRmVmssVM -ResourceGroupName $resourceGroup -VMScaleSetName $vmssName -InstanceId $vm.InstanceID -InstanceView
     # Write-Host $vm_instance_view.Statuses[1].Code
-    if ($vm_instance_view.Statuses[1].Code -eq 'PowerState/deallocated')
-    {
-        Write-Host 'Starting VM ID:' $vm.InstanceID
-        start-azurermvmss -ResourceGroupName $resourceGroup -VMScaleSetName $vmssName -InstanceId $vm.InstanceID
-        $started++
+    if ($vm_instance_view.Statuses[1].Code -eq 'PowerState/deallocated') {
+        if ($count -gt 0) {
+            $id_string += ','
+        }
+        $id_string += $vm.InstanceID -as [string]
+        $count++
     }
-    if ($started  -eq $startCount) 
-    {
-        Write-Host 'Started ' $started ' instances'
+    if ($count -eq $startCount) {
         break
     }
+}
+if ($count -gt 0) {
+    Write-Host 'Starting ' $count ' instances: ' $id_string
+    Invoke-Expression -Command "start-azurermvmss -ResourceGroupName $resourceGroup -VMScaleSetName $vmssName -InstanceId $id_string"
+}
+else {
+    Write-Host 'No deallocated instances found in scale set: ' $vmssName
 }
